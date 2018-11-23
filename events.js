5235cff9458b7f4746c2d026579c7e8b8a708988
liamLatour <38808200+liamLatour@users.noreply.github.com>
2018-11-23 08:27:55 +0100

    Better Excel and more responsive

--- events.js
+++ events.js
@@ -170,8 +170,10 @@ $("#End").on('click', function(){
 
 $("#menu").on('click', 'td', function(){
     $("#t"+id).css('display', 'none');
+    $("#"+id).removeClass('highlight');
 
     id = $(this).attr('id');
+    $("#"+id).addClass('highlight');
     $("#t"+id).css('display', 'table');
 });
 
@@ -265,6 +267,8 @@ $("#toExcel").on('click', function(){
         if(item[0] == 'C'){
             let obj = JSON.parse(localStorage.getItem(item));
             totalCmd++;
+            let reducPour = 0;
+            let sousTotal = 0;
             for(let product in obj){
                 try{
                     let id = -1;
@@ -272,37 +276,78 @@ $("#toExcel").on('click', function(){
                     let thisCost = 0;
 
                     if(!isNaN(product)){
+                        if(products[product][1] < 0 && products[product][2] === 'P'){
+                            reducPour = obj[product];
+                        }
                         thisName = products[product][0];
                         thisCost = products[product][1];
                     }
                     else if(product.includes('M')){
                         thisName = "Menu "+formules[product[1]][0];
                         thisCost = formules[product[1]][2];
-                    }
-                    else{
-                        thisName = "Formule "+formules[product[2]][0];
-                        thisCost = formules[product[2]][1];
-                    }
-
-                    // Check if already here
-                    for(let i=0; i<ws_data.length; i++){
-                        if(ws_data[i][0] == thisName){
-                            id = i;
-                            break;
+                        if(product.includes('B')){
+                            thisName += " + Boisson";
+                            thisCost += products[Number(product.substring(product.length-1, product.length))+6][1];
                         }
                     }
-
-                    if(id != -1){
-                        ws_data[id][1]+=1;
-                        ws_data[id][3]+=thisCost;
+                    else if(product.includes('F')){
+                        thisName = "Formule "+formules[product[2]][0];
+                        thisCost = formules[product[2]][1];
+                        if(product.includes('B')){
+                            thisName += " + Boisson";
+                            thisCost += products[Number(product.substring(product.length-1, product.length))+6][1];
+                        }
                     }
                     else{
-                        ws_data.push([thisName, 1, thisCost, thisCost]);
+                        continue;
                     }
-                    totalNb++;
-                    totalMoney+=thisCost;
+
+                    if(thisCost > 0 || products[product][2] == 'E'){
+                        // Check if already here
+                        for(let i=0; i<ws_data.length; i++){
+                            if(ws_data[i][0] == thisName){
+                                id = i;
+                                break;
+                            }
+                        }
+
+                        if(id != -1){
+                            ws_data[id][1] += obj[product];
+                            ws_data[id][3] += thisCost * obj[product];
+                        }
+                        else{
+                            ws_data.push([thisName, obj[product], thisCost, thisCost * obj[product]]);
+                        }
+
+                        totalMoney+=thisCost * obj[product];
+                        sousTotal+=thisCost * obj[product];
+
+                        if(thisCost > 0){
+                            totalNb++;
+                        }
+                    }
                 }
                 catch{}
+            }
+
+            if(reducPour > 0){
+                let id = -1;
+                // Check if already here
+                for(let i=0; i<ws_data.length; i++){
+                    if(ws_data[i][0] == products[18][0]){
+                        id = i;
+                        break;
+                    }
+                }
+                let reduc = sousTotal/100*Math.abs(reducPour);
+                if(id != -1){
+                    ws_data[id][1] += 1;
+                    ws_data[id][3] -= reduc;
+                }
+                else{
+                    ws_data.push([products[18][0], 1, -1, -reduc]);
+                }
+                totalMoney -= reduc;
             }
         }
     }
@@ -313,12 +358,12 @@ $("#toExcel").on('click', function(){
     var ws = XLSX.utils.aoa_to_sheet(ws_data);
 
     var wscols = [
-        {wch:20},
-        {wch:15},
+        {wch:25},
+        {wch:14},
         {wch:8},
         {wch:8},
         {wch:7},
-        {wch:15},
+        {wch:13},
         {wch:15}
     ];
 
@@ -328,15 +373,26 @@ $("#toExcel").on('click', function(){
     var wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
 
     function s2ab(s) { 
-        var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
-        var view = new Uint8Array(buf);  //create uint8array as viewer
-        for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
         return buf;    
     }
 
     function sortCommand(a, b) {
+        if(a[2] < 0){
+            return 1;
+        }
+        else if(b[2] < 0){
+            return -1;
+        }
         if (a[1] === b[1]) {
-            return 0;
+            if(a[2] === b[2]){
+                return 0;
+            }
+            else{
+                return (a[2] < b[2]) ? 1 : -1;
+            }
         }
         else {
             return (a[1] < b[1]) ? 1 : -1;
